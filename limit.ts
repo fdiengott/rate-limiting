8a86dfc525079fed4b24567550844e71ff11ea31
fdiengott <freddy.diengott@proton.me>
2024-04-11 23:28:33 -0400

    setup rate limiting

--- limit.ts
+++ limit.ts
@@ -6,6 +6,18 @@ const port = 3000;
 // initialize an Express server
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 10, // each IP can make up to 10 requests perf 5 min
+    standardHeaders: true,
+    legacyHeaders: false,
+    // __defaults__
+    // message: "Too many requests, please try again later."
+    // statusCode: 429
+});
+
+app.use(limiter);
+
 // define a sample endpoint
 app.get("/hello-world", (req, res) => {
     res.send("Hello, World!");
